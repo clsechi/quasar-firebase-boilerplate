e0f09770f515cb464e00dd2478a8b5f9b6ba1584
Carlos Sechi <carlos.sechi@frentecorretora.com.br>
2019-04-07 03:11:15 -0300

    Remove firebase default configs
    Add comments to explain better how each feature works and how add new

--- src/plugins/Firebase.js
+++ src/plugins/Firebase.js
@@ -2,22 +2,41 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+/**
+ * You can read more about Firebase setup here
+ * https://firebase.google.com/docs/web/setup
+ */
+
 const config = {
-  apiKey: 'AIzaSyDz7HEWdU8GHO70NbGmH2DZfvyaGEzITYQ',
-  authDomain: 'car-rent-control.firebaseapp.com',
-  databaseURL: 'https://car-rent-control.firebaseio.com',
-  projectId: 'car-rent-control',
-  storageBucket: 'car-rent-control.appspot.com',
-  messagingSenderId: '64153021131',
+  apiKey: '{your-project-api-key}',
+  authDomain: '{your-project-id}.firebaseapp.com',
+  databaseURL: 'https://{your-project-id}.firebaseio.com',
+  projectId: '{your-project-id}',
+  storageBucket: '{your-project-id}.appspot.com',
+  messagingSenderId: '{your-message-sender-id}',
 };
 
 firebase.initializeApp(config);
-
+/**
+ * remove if you dont want to use local Firestore Auth
+ * or update like you ish
+ * read more: https://firebase.google.com/docs/auth/web/auth-state-persistence
+ */
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
 
 firebase.firestore().settings({ timestampsInSnapshots: true });
+
+/**
+ * remove if you dont want to use local Firestore persistence
+ * read more: https://firebase.google.com/docs/firestore/manage-data/enable-offline
+ */
 firebase.firestore().enablePersistence();
 
+/**
+ * Add your Firebase features as you need
+ * Use Vue prototype to have access to these features
+ * in any part of your code
+ */
 export default ({ Vue }) => {
   Vue.prototype.$firebase = firebase;
   Vue.prototype.$firestore = firebase.firestore();
